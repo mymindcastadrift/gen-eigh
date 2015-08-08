Case 1 & 2 Working. Scipy performance suspected to be due to LAPACK scaling/guard digit mechanism, and is not native to the algorithm Case 3 major bug due to questionable instructions in book

choleskywilkinson.py
@@ -7,13 +7,11 @@ from scipy import linalg
 
 def cholesky_wilkinson(A, M): #A and M are both NumPy Matrix Objects
 	L =  np.matrix(linalg.cholesky(M))
-	S = np.matrix(linalg.solve_triangular(L, A))   # S = LG
-	G = (np.matrix(linalg.solve_triangular(L, S.getH()))).getH()
+	S = np.matrix(linalg.solve_triangular(L, A))   # S = L-1 A    Lh X
+	G = (np.matrix(linalg.solve_triangular(L, S.getH()))).getH()  # G = S L-H   Lh X
 	[eigenval, eigenvect] = linalg.eigh(G)
-	eigenvect = np.matrix(linalg.solve_triangular(L, eigenvect, trans = 'C'))
-	eigenval = np.matrix(eigenval)
-	return (eigenval, eigenvect)
-
+	eigenvect = np.matrix(linalg.solve_triangular(L, eigenvect, trans = 'T')) # Eigenvect = L-H Lh X
+	return eigenval, eigenvect
 
 
 # Key Areas of Improvement:

fixheiberger.py
@@ -29,7 +29,7 @@ def find_contraction_index(eigenval, r):
 	index = index + 1
 	return index
 
-def sort_eigenval(eigenvect, eigenval): 
+def sort_eigenval(eigenvect, eigenval, p = False): 
 
 	def getkey(item):
 		return abs(item[0])
@@ -67,18 +67,20 @@ def fix_heiberger(A,M, r, cond = True):
 	index = find_contraction_index(d, r)
 	d[index:] = [0 for e in d[index:]]
 	A = G.getH()* A * G
+	
 	d_root_inv = 1/np.sqrt(d[0:index])
+	D = np.matrix(np.diag(d_root_inv))
+	D = linalg.block_diag(D, np.identity(dim - index))
+	A = D*A*D 
 
 	if (index > dim - 1 ):
 		
 		# If M is well-conditioned, simply solve the newly decomposed problem.
 		print "Case 1"
-		U = np.matrix(np.diag(d_root_inv))
-		Z = U.getH()*A*U
-		[eigenval, eigenvect] = linalg.eigh(Z)
+		[eigenval, eigenvect] = linalg.eigh(A)
 
 		# Reconstruct eigenvectors
-		return eigenval, G * U * eigenvect
+		return eigenval, G * D * eigenvect
 
 	else:
 		
@@ -86,37 +88,36 @@ def fix_heiberger(A,M, r, cond = True):
 		# Deflation Proedure on A_22
 		A_22 = A[index:dim, index:dim]
 		[z,F] = linalg.eigh(A_22)
-		F = sort_eigenval(F,z)
+		F = sort_eigenval(F,z, True)
 		index_a = find_contraction_index(z, r)
 
 		z[index_a:] = [0 for e in z[index_a:]]
 
-		U = np.matrix(linalg.block_diag(np.diag(d_root_inv), F))
-		Z = U.getH()*A*U
-		Z[index: , index: ] = np.diag(z)
+		U = np.matrix(linalg.block_diag(np.identity(index), F))
+		A = U.getH()*A*U
+		A[index: , index: ] = np.diag(z)
 
-		if (index_a > A_22.shape[0]-1):
+		if (index_a > len(z)-1):
 			
 			print "Case 2"
 			# When A_22 is well-conditioned
-			A_12 = Z[0:index, index:dim]
-			A_11 = Z[0:index, 0:index]
+			A_12 = A[0:index, index:dim]
+			A_11 = A[0:index, 0:index]
 			Psi_inv = np.diag(1/z)
 			[eigenval, x_1] = linalg.eigh(A_11 - A_12 * Psi_inv * A_12.getH())
 
 			# Reconstruct eigenvectors
-			z_inv = 1/z[0:index_a]
-			x_2 = -np.diag(z_inv)* A_12.getH() * x_1
+			x_2 = - Psi_inv * A_12.getH() * np.matrix(x_1)
 			eigenvect = np.concatenate((x_1,x_2), axis=0)
-			return eigenval, U * G * eigenvect
+			return eigenval, G * D * U * eigenvect
 
 		else:
 
 			print "Case 3"
 
 			# General case: Possibly singular A_13
+			# TODO Assert index_a < dim - index - index_a
 			A_13 = Z[0:index, index + index_a: dim]
-			#print "A_13:", A_13
 
 			[Q,s,Ph] = linalg.svd(A_13)
 			index_b = find_contraction_index(s,r)
@@ -124,14 +125,14 @@ def fix_heiberger(A,M, r, cond = True):
 			I = np.matlib.identity(index_a)
 			V = np.matrix(linalg.block_diag(Q, I, (np.matrix(Ph)).getH()))
 			
-			B = V * Z * V.getH()
-			B[0:index, index+index_a:dim] = np.zeros((index, dim - index - index_a))
-			B[0:len(s), index+index_a: index+index_a+len(s)] = np.diag(s)
-			B[index+index_a:dim, 0:index] = np.matrix(B[0:index, index+index_a:dim]).getH()
+			A = V * A * V.getH()
+			A[0:index, index+index_a:dim] = np.zeros((index, dim - index - index_a))
+			A[0:len(s), index+index_a: index+index_a+len(s)] = np.diag(s)
+			A[index+index_a:dim, 0:index] = np.matrix(A[0:index, index+index_a:dim]).getH()
 
 			# Solve the equivalent problem
-			B_22 = B[index_b: index, index_b: index]
-			B_23 = B[index_b: index, index: index+index_a]
+			B_22 = A[index_b: index, index_b: index]
+			B_23 = A[index_b: index, index: index+index_a]
 			Psi_inv = np.diag(1/z[0:index_a])
 			[eigenval, x_2] = linalg.eigh(B_22 - B_23 * Psi_inv * B_23.getH())
 
@@ -142,8 +143,8 @@ def fix_heiberger(A,M, r, cond = True):
 			x_3 = -np.diag(z_inv)* B_23.getH() * x_2
 
 			s_inv = 1/s[0:index_b]
-			B_12 = B[0:index_b, index_b: index]
-			B_13 = B[0:index_b, index: index + index_a]
+			B_12 = A[0:index_b, index_b: index]
+			B_13 = A[0:index_b, index: index + index_a]
 			x_4 = -np.diag(s_inv) * (B_12*x_2 + B_13*x_3)
 
 			x_5 = np.zeros((dim - index - index_a - index_b, len(eigenval)))
@@ -159,12 +160,23 @@ if __name__ == "__main__":
 
 		[fh_val, fh_vect] = fix_heiberger(A,M,r)
 		[def_val, def_vect] = linalg.eigh(A,M)
+		[cw_val, cw_vect] = cholesky_wilkinson(A,M)
 		print "Fix-Heiberger with error: ", average_error(A,M, fh_val, fh_vect)
+		print "Cholesky-Wilkinson with error: ", average_error(A,M, cw_val, cw_vect)
 		print "Scipy with error: ", average_error(A,M, def_val, def_vect), "\n"
 
 	# Testing for Case 1 Performance
-
+	print "Testing Case 1 \n"
 	for i in range(2,8):
 		A = matgen.rand_symm(20*i)
-		M = matgen.rand_semidef_symm(20*i)
-		run_test(A,M,0.0000001)
+		M = matgen.rand_by_eigenrange(20*i, 1,100000)
+		run_test(A,M,0.0000011)
+
+	# Testing for Case 2 Performance
+
+	print "Testing Case 2\n"
+	for i in range(2,8):
+		A = matgen.rand_symm(100*i)
+		M = matgen.rand_by_eigenrange(100*i, 0,10000000)
+		run_test(A,M, 0.01)
+